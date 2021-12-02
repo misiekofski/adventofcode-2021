6537c2892e121f703facdd7fdde18ad0f80fe5c0
Michal Dobrzycki <dobrzycki@gmail.com>
2021-12-02 18:13:49 +0100

    Added Day2 solution in Scala

---
+++ src/main/Day01.scala
@@ -0,0 +1,19 @@
+import scala.io.Source
+
+object Day01 {
+  val lines = Source
+    .fromResource("day1.txt")
+    .getLines
+    .map(_.toInt)
+    .toSeq
+
+  // https://docs.scala-lang.org/overviews/collections/trait-iterable.html#inner-main
+  def part1(input: Seq[Int]) = input.sliding(2).count { case Seq(x, y) => y > x }
+
+  def part2(input: Seq[Int]) = part1(input.sliding(3).map(_.sum).toSeq)
+
+  def main(args: Array[String]): Unit = {
+    println(part1(lines))
+    println(part2(lines))
+  }
+}
